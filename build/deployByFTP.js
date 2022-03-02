d5283fd0e8e001eae3f838eba02fbcbb4f7427eb
huypham <huyquansu96@gmail.com>
2022-03-03 01:15:20 +0700

    fix

--- build/deployByFTP.js
+++ build/deployByFTP.js
@@ -48,7 +48,7 @@ c.on("ready", async ()=> {
         await new Promise((resolve, reject)=>{
             setTimeout(()=>{
                 resolve(true) ;
-            },1100);
+            },1000);
         });
         c.put(fi,"/site/wwwroot/bin/public/"+(fi.replace(path.join(__dirname,"..","azure","bin","public"),"")),(e)=>{
             console.log(e);
