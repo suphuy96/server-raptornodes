21f6362bbeb5ab3d25ad4cfd9e44d04dc6d6d0f4
suphuy96 <suphuy96@gmail.com>
2022-04-22 05:00:06 +0700

    support MONGODB_URI

--- src/helper/backup/mongodb.ts
+++ src/helper/backup/mongodb.ts
@@ -3,7 +3,7 @@ import {spawn} from "child_process";
 import moment from "moment";
 import {MONGODB_URI, DB_NAME_MANNET, ENVIRONMENT} from "../../util/secrets";
 export function  getPathFile():string {
-    const dir = path.join(__dirname,"..","..","..","src","backup");
+    const dir = path.join(__dirname,"..","backup");
     return dir;
 }
 
@@ -11,7 +11,7 @@ export function reStoreBK  (pathZip:string):Promise<string>{
         return new Promise((resolve)=>{
         console.log("hêyreStoreBK");
             const dbName = ENVIRONMENT === "production"?DB_NAME_MANNET:process.env.DB_NAME_TESTNET;
-            const dir = path.join(__dirname,"..","..","..","src","backup",pathZip);
+            const dir = path.join(__dirname,"..","backup",pathZip);
     console.log(dir,"dir");
     const backupProcess = spawn("mongorestore", [
          "--uri="+MONGODB_URI,
@@ -37,7 +37,7 @@ export default  (name:string):Promise<string>=>{
     return new Promise((resolve)=>{
         const dbName = process.env.DB_NAME;
         const day = moment(new Date()).format("DD-MM-YYYY_HH_mm");
-        const dir = path.join(__dirname,"..","..","..","src","backup",name);
+        const dir = path.join(__dirname,"..","backup",name);
         console.log(dir,"dir");
         const backupProcess = spawn("mongodump", [
             "--db="+dbName,
